9123737e3329c6617725b0f9e52157b3e29fad7a
Hyunjin Yang <yang.skkuice@gmail.com>
2015-02-02 15:43:00 +0900

    150202_중간정검
    150202_중간정검 opencv 넣기

---
+++ camera_test/src/com/example/camera_test/history.java
@@ -0,0 +1,18 @@
+package com.example.camera_test;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class history extends Activity {
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    
+	    setContentView(R.layout.history_activity);
+	
+	 
+	}
+
+}
